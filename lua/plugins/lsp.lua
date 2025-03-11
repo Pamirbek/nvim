fb5b579b6b8c18a20aee1bec5746ffaff50ad1a7
Pamcho <pauchok006@gmail.com>
2025-03-11 11:39:23 +0100

    Add snippets and minor changes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,7 +18,7 @@ return {
         vim.api.nvim_create_autocmd('LspAttach', {
             desc = "LSP Actions",
             callback = function(args)
-                -- vim.keymap.set("n", "K", vim.lsp.buf.hover, {noremap = true, silent = true})
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, {noremap = true, silent = true})
                 vim.keymap.set("n", "gd", vim.lsp.buf.definition, {noremap = true, silent = true})
             end,
         })
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -7,47 +7,57 @@ return {
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-buffer",
+
+            "hrsh7th/cmp-vsnip",
+            "hrsh7th/vim-vsnip",
+            "rafamadriz/friendly-snippets",
         },
         config = function()
-            local cmp = require('cmp')
-            local luasnip = require('luasnip')
-            local opts = {
-                sources = cmp.config.sources {
-                    { name = "nvim_lsp", },
-                    { name = "path", },
-                    { name = "buffer", },
+            local cmp = require"cmp"
+
+            local has_words_before = function()
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            end
+
+            local feedkey = function(key, mode)
+              vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+            end
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        vim.fn["vsnip#anonymous"](args.body)
+                    end,
                 },
                 mapping = cmp.mapping.preset.insert({
                     ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                    ["<tab>"] = cmp.mapping(function(original)
-                        print("tab pressed")
-                        if cmp.visible() then
-                            print("cmp expand")
-                            cmp.select_next_item()
-                        elseif luasnip.expand_or_jumpable() then
-                            print("snippet expand")
-                            luasnip.expand_or_jump()
-                        else
-                            print("fallback")
-                            original()
-                        end
-                    end, {"i", "s"}),
-                    ["<S-tab>"] = cmp.mapping(function(original)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.expand_or_jumpable() then
-                            luasnip.jump(-1)
-                        else
-                            original()
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if vim.fn["vsnip#available"](1) == 1 then
+                            feedkey("<Plug>(vsnip-jump-next)", "")
                         end
                     end, {"i", "s"}),
 
+                    ["<S-Tab>"] = cmp.mapping(function()
+                        if vim.fn["vsnip#jumpable"](-1) == 1 then
+                            feedkey("<Plug>(vsnip-jump-prev)", "")
+                        end
+                    end, {"i", "s"})
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "vsnip" },
+                }, {
+                    { name = "buffer" },
+                }, {
+                    { name = "path" },
                 })
-            }
-            cmp.setup(opts)
+            })
         end,
     },
     { "hrsh7th/cmp-nvim-lsp", lazy = true },
     { "hrsh7th/cmp-path", lazy = true },
     { "hrsh7th/cmp-buffer", lazy = true },
+    { "hrsh7th/cmp-vsnip", lazy = true },
+    { "hrsh7th/vim-vsnip", lazy = true },
 }
